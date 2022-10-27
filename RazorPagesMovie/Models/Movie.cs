31b27021b96525f536fded9c70bef3d51b70cff4
hiro-suzuki-pe <suzuki.hiroyuki@mbr.nifty.com>
2022-10-27 14:57:40 +0900

    「検索の追加」まで完成した

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,5 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -8,9 +8,14 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
 
+        [Display(Name = "リリース日")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "ジャンル")]
         public string Genre { get; set; } = string.Empty;
+
+        [Column(TypeName ="decimal(18,4)")]
         public decimal Price { get; set; }
     }
 }
