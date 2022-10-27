ddd219cd54f01c7eba2903ff571027f6dd54aaa2
Hiroyuki Suzuki <suzuki.hiroyuki@mbr.nifty.com>
2022-10-28 06:24:02 +0900

    検証の追加まで実施

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,16 +6,28 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength =3)]   
         public string Title { get; set; } = string.Empty;
 
         [Display(Name = "リリース日")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [Display(Name = "ジャンル")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; } = string.Empty;
 
-        [Column(TypeName ="decimal(18,4)")]
-        public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
+        public string Rating { get; set; } = string.Empty;
     }
 }
